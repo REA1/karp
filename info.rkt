37d5afaaf550c1419f03054c9415285727b6ffb5
Chenhao Zhang <chenhao.zhang.rea@u.northwestern.edu>
2023-04-26 18:02:11 -0500

    added build doc dependencies

--- info.rkt
+++ info.rkt
@@ -1,7 +1,8 @@
 #lang info
 (define collection "karp")
 (define deps '("base" "rosette"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"
+		     "scribble-math"))
 (define scribblings '(("scribblings/karp.scrbl" (multi-page))))
 (define pkg-desc "Karp -- a language NP reductions")
 (define version "0.9")
